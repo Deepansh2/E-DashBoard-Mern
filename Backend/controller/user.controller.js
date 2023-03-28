49fb1d20fdbd748599e6e0a1d69b91c43ab8d709
Deepanshu <deepanshusing54@gmail.com>
2023-03-28 12:27:04 +0530

    added login api

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -2,7 +2,7 @@ const User = require("../model/user.model");
 
 
 
-exports.create = async (req,res) =>{
+exports.signup = async (req,res) =>{
 
     const userObj = {
         name : req.body.name,
@@ -13,12 +13,62 @@ exports.create = async (req,res) =>{
     try{
     const user = await User.create(userObj);
     console.log(`new user ${user.name} created`)
+    const response = {
+        name : user.name,
+        email : user.email
+    }
     return res.status(201).send({
         message : "New User Created",
-        User : user
+        User : response
     })
 }catch(err){
     console.log("Error while creating user",err)
+    return res.status(500).send({
+        message : "Internal server error"
+    })
+}
+}
+
+
+exports.signin = async (req,res) =>{
+
+    try{
+
+    if(req.body.email){
+        return res.status(400).send({
+            message : "Email is not provided"
+        })
+    }
+    if(req.body.password){
+        return res.status(400).send({
+            message : "Password is not Provided"
+        })
+    }
+    
+    const user = await User.findOne({email:req.body.email})
+    if(user){
+        if(user.password == req.body.password){
+            const response = {
+                id : user._id,
+                name : user.name,
+                email : user.email
+            }
+            return res.status(200).send({
+                message : "LoggedIn Successfully",
+                user : response
+            })
+        }else{
+            return res.status(400).send({
+                message : "Wrong Password"
+            })
+        }
+    }else{
+        return res.status(400).send({
+            message : "User doesn't exist"
+        })
+    }
+}catch(err){
+    console.log("Error while login",err)
     return res.status(500).send({
         message : "Internal server error"
     })
--- Backend/route/user.route.js
+++ Backend/route/user.route.js
@@ -3,5 +3,6 @@ const userController = require("../controller/user.controller");
 
 module.exports = (app) =>{
 
-    app.post("/mern/user",userController.create)
+    app.post("/mern/user/signup",userController.signup)
+    app.post("/mern/user/signin",userController.signin)
 }
