6dc8c60f144a4953cdb2dd640f44c0988609f2a9
Deepanshu <deepanshusing54@gmail.com>
2023-03-29 11:05:37 +0530

    navBar updated

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -10,21 +10,24 @@ const Nav = () => {
     }
     return (
         <div>
-            <ul className="nav-ul" style={{ padding: "5px", margin: "0px", background: "gainsboro" }}>
-                <li> <Link to="/">Products</Link></li>
-                <li> <Link to="/add">Add Product</Link></li>
-                <li> <Link to="/update">Update Product</Link></li>
-                <li> <Link to="/profile">Profile</Link></li>
+            <img 
+            alt="logo"
+            className="logo"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcdWBkuZRnRjV4iI72Vf58QYB99mvNYdmLrw&usqp=CAU"/>
+           {auth ?<ul className="nav-ul">
                 {/* <li>{auth ? <Link onClick={logout} to="/signup">Logout</Link> : <Link to="/signup">Signup</Link>}</li>
                 <li> <Link to="/login">Login</Link></li> */}
-                {
-                    auth ? <li><Link onClick={logout} to="/signup">Logout</Link></li>
-                    :<>
-                    <li><Link to="/signup">Signup</Link></li>
-                    <li> <Link to="/login">Login</Link></li>
-                </>
-                }
-            </ul>
+                        <li> <Link to="/">Products</Link></li>
+                        <li> <Link to="/add">Add Product</Link></li>
+                        <li> <Link to="/update">Update Product</Link></li>
+                        <li> <Link to="/profile">Profile</Link></li>
+                        <li><Link onClick={logout} to="/signup">Logout ({JSON.parse(auth).user.name})</Link></li>
+                    </ul> :
+                    <ul className="nav-ul nav-right">
+                        <li><Link to="/signup">Signup</Link></li>
+                        <li> <Link to="/login">Login</Link></li>
+                    </ul>
+            }
         </div>
     )
 }
