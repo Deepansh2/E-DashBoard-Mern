9cf250e5ace2a9090723b0d574afb588b09957d0
Deepanshu <deepanshusing54@gmail.com>
2023-03-29 14:57:20 +0530

    simple form validation on Product added

--- frontend/src/components/AddProduct.js
+++ frontend/src/components/AddProduct.js
@@ -8,7 +8,14 @@ const AddProduct = ()=>{
     const [category,setCategory] = useState("")
     const [company,setCompany] = useState("")
     const [description,setDescription] = useState("")
+    const [error,setError] = useState(false)
     const SaveProduct = async ()=>{
+
+        console.warn(!name)
+        if(!name || !price || !category || !company || !description){
+            setError(true)
+            return false
+        }
         const userId = JSON.parse(localStorage.getItem("user")).user.id
         const Data = {name,price,category,company,description,userId}
     
@@ -26,10 +33,15 @@ const AddProduct = ()=>{
         <div className="addProduct">
             <h1>Add Product</h1>
             <input className="inputBox" type="text"  placeholder="Enter Product Name" value={name} onChange={(e)=>setName(e.target.value)} />
+            {error && !name && <span className="invalid-input">Enter valid name</span>}
             <input className="inputBox" type="text" placeholder="Enter Product Price" value={price} onChange={(e)=>setPrice(e.target.value)} />
+            {error && !price && <span className="invalid-input">Enter valid price</span>}
             <input className="inputBox" type="text" placeholder="Enter Product Category" value={category} onChange={(e)=> setCategory(e.target.value)} />
+            {error && !category && <span className="invalid-input">Enter valid category</span>}
             <input className="inputBox" type="text" placeholder="Enter Product Company Name" value={company} onChange={(e)=>setCompany(e.target.value)} />
+            {error && !company && <span className="invalid-input">Enter valid compony</span>}
             <input className="inputBox" type="text" placeholder="Enter Product Description" value={description} onChange={(e) => setDescription(e.target.value)} />
+            {error && !description && <span className="invalid-input">Enter valid description</span>}
             <button className="appButton" type="button" onClick = {SaveProduct}>Save</button>
         </div>
     )
